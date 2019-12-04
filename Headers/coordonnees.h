28948759a6929a755ef2137e1158088af59fc3b2
Matal <dovahmatt917@gmail.com>
2019-12-04 13:53:37 +0100

    Ajout de coordonnes.h

---
+++ Headers/coordonnees.h
@@ -0,0 +1,10 @@
+
+#define TAILLE_NOM 20
+#define TAILLE_MAIL 40
+
+typedef struct {
+	char nom[TAILLE_NOM];
+	char prenom[TAILLE_NOM];
+	char addresse_mail[TAILLE_MAIL];
+	float importance;	
+}COORDONNEES;
---
+++ Sources/mainHotelBooking.c
@@ -0,0 +1,15 @@
+/*
+ * mainHotelBooking.c
+ * 
+ */
+
+
+#include <stdio.h>
+#include "../Headers/coordonnees.h"
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
