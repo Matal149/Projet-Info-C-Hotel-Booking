3dd4ac783742840a32b271a0a83ba9b36951c96e
Matal <dovahmatt917@gmail.com>
2019-12-04 15:39:54 +0100

    Ajout dans Headers du fichiers keyxords.h

--- Headers/coordonnees.h
+++ Headers/coordonnees.h
@@ -5,6 +5,6 @@
 typedef struct {
 	char nom[TAILLE_NOM];
 	char prenom[TAILLE_NOM];
-	char addresse_mail[TAILLE_MAIL];
+	char adresse_mail[TAILLE_MAIL];
 	float importance;	
 }COORDONNEES;
---
+++ Headers/keywords.h
@@ -0,0 +1,8 @@
+
+#define NB_VILLES 10
+#define TAILLE_VILLES 20
+
+typedef struct{
+	char villes[NB_VILLES][TAILLE_VILLES] = {"Toulon", "Paris", "Lyon", "Poitiers", "Orange", "Toulouse", "Caen", "Marseille", "Rouen", "Troyes"};
+	char types_resrvation[10][25] = {"week-end","vacances"}; 
+}KEYWORDS;
