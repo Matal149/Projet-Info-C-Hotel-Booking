79b91873327a00e998395b73fffc8e9deab6ad21
ouagadoudou <antoine.mah83@gmail.com>
2019-12-12 08:48:21 +0100

    modif main

--- Sources/mainHotelBooking.c
+++ Sources/mainHotelBooking.c
@@ -5,13 +5,14 @@
 
 
 #include <stdio.h>
-#include "../Headers/coordonnees.h"
 #include "../Headers/gestion_de_fichier.h"
 #include "../Headers/e_mail.h"
 #include "../Headers/keywords.h"
 int main(int argc, char **argv)
 {
 	
+	FILE*nom_fichier=NULL;
+	nom_fichier=ouvrir("coordonnees.bin");
 	return 0;
 }
 
