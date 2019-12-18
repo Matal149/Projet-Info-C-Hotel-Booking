3a9e86ae5fc9e51caecddb62bce86950958623bb
Matal <dovahmatt917@gmail.com>
2019-12-18 14:34:43 +0100

    Test ajout fonction de suppression de donnees

--- Sources/gestion_de_fichier.c
+++ Sources/gestion_de_fichier.c
@@ -1,6 +1,7 @@
 #include "../Headers/gestion_de_fichier.h"
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #define DEMANDE_CONFIRMATION 0
 
 FILE*ouvrir(char*nom_fichier)
@@ -139,3 +140,89 @@ void affichercoordonnees(COORDONNEES*coordonnees)
 	printf("prenom : %s\n",coordonnees->prenom);
 	printf("adresse mail : %s\n",coordonnees->adresse_mail);
 }
+
+
+void supprimerCoordonnes(FILE* fichier){
+	COORDONNEES coordonnees;
+	char nom_recherche[TAILLE_NOM];
+	char prenom_recherche[TAILLE_NOM];
+	char choix;
+	printf("Supprimer\n");
+	
+	printf("choix du critere de recherche pour supprimer une coordonnee :\n");
+	printf("1 : nom | 2 prenom \n");
+	scanf("%c",&choix);getchar();
+	switch(choix)
+	{
+		case '1':
+			printf("saisi du nom à supprimer : ");
+			scanf("%s",nom_recherche);getchar();
+			break;
+			
+		case '2':
+			printf("saisi du prenom a rechercher : ");
+			scanf("%s",prenom_recherche);getchar();
+			break;
+			
+		default :
+			printf("critere inconnu : abandon \n");
+			return;
+	}
+	
+/*positionnement du curseur au debut du ficher */
+	fseek(fichier ,0,SEEK_SET);
+/* on va lire des CLIENT du fichier un par un jusqua la fin du fichier*/
+	while(fread(&coordonnees,sizeof(COORDONNEES),1,fichier)!=0)
+	{	
+/* pour chaque mode de recherche on va appliquer la bonne comparaison*/
+		if(choix == '1')
+		{
+			if(strcmp(nom_recherche,coordonnees.nom)==0)
+			{
+				printf("coordonnees trouve\n");
+				//~ affichercoordonnees(&coordonnees);
+				//~ ftruncate(int fildes, sizeof(COORDONNEES));
+				fwrite(&new_coordonnees,sizeof(COORDONNEES),1,fichier);
+				return;
+			}
+		}
+		else if ( choix == '2')
+		{
+			if(strcmp(prenom_recherche,coordonnees.prenom)==0)
+			{
+				printf("coordonnees trouve\n");
+				//~ affichercoordonnees(&coordonnees);
+				
+				return;
+			}
+		}
+	}
+/* si on est arrive ici on n'a donc pas trouver le client */	
+	printf("Client introuvable \n");
+}
+
+
+//~ void modifier_base_de_donnees(FILE *donnees)
+//~ {
+    //~ char mot_cle[30];
+    //~ char mot_a_modifier[30];
+    //~ char nouveau_mot[30];
+    //~ char const *ancien_nom = "coordonnees.bin";
+    //~ char const *nouveau_nom = "coordonnees2.bin";
+    //~ FILE *coordonnees2;
+ 
+    //~ coordonnees2 = fopen(nouveau ,"w");
+    //~ printf("entrez le mot que vous voulez changer\n");
+    //~ scanf("%s",mot_a_modifier);
+    //~ printf("entrez le nouveau mot\n");
+    //~ scanf("%s",nouveau_mot);
+ 
+    //~ while (fscanf(donnees, "%s", mot_cle) == 1)
+        //~ if (strcmp(mot_cle, mot_a_modifier) == 0)
+            //~ fprintf(coordonnees2, "%s\n", mot_a_modifier);
+        //~ else
+            //~ fprintf(coordonnees2, "%s\n", mot_cle);
+ 
+    //~ remove(ancien);
+    //~ rename(ancien, nouveau);
+//~ }
