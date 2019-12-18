049e0b74dedc78840ecdc7700410eee6fd849061
Matal <dovahmatt917@gmail.com>
2019-12-18 08:41:37 +0100

    Modification du menu général

--- Sources/mainHotelBooking.c
+++ Sources/mainHotelBooking.c
@@ -9,34 +9,40 @@
 #include "../Headers/e_mail.h"
 #include "../Headers/keywords.h"
 
-void menu(FILE *fichier);
+void menuGeneral(FILE* fichier_coordonnees, FILE* fichier_keywords);
 
 int main(int argc, char **argv)
 {
 	/* Test fontion gestion_de_fichier.c*/
-	//~ FILE*fichier_coordonnes=NULL;
-	//~ fichier_coordonnes = ouvrir("coordonnees.bin");
+	FILE*fichier_coordonnees=NULL;
+	FILE*fichier_keywords=NULL;
+
+	fichier_coordonnees = ouvrir("coordonnees.bin");
+	fichier_keywords = ouvrir("reponses.bin");
 	//~ ajout(fichier_coordonnes);
 	//~ affiche(fichier_coordonnes);
 	//~ lister(fichier_coordonnes);
 	
 	
-	//~ menu(fichier_compte);
+	menuGeneral(fichier_coordonnees, fichier_keywords);
 	
 	return 0;
 }
 
 
 
-void menu(FILE *fichier)
+void menuGeneral(FILE* fichier_coordonnes, FILE* fichier_keywords)
 {
 	char choix;
 	do
 	{
-		printf("Ajouter d'un nouveau client............: A\n");
-		printf("Consultation d'un compte client........: C\n");
-		printf("Lister tous les comptes de clients.....: L\n");
-		printf("Opération sur un compte client.........: O\n");
+		//~ printf("Ajouter d'un nouveau client............: A\n");
+		//~ printf("Consultation d'un compte client........: C\n");
+		//~ printf("Lister tous les comptes de clients.....: L\n");
+		//~ printf("Opération sur un compte client.........: O\n");
+		
+		printf("Choix du mode d'utilisation :\n");
+		printf("1 : administrateur | 2 utilisateur \n");
 		printf("Quitter................................: Q\n");
 		printf(" votre choix: ");
 
@@ -45,24 +51,14 @@ void menu(FILE *fichier)
 		
 		switch(choix)
 		{
-			case 'a':
-			case 'A': 
-				ajout(fichier);
+			case '1': 
+				//~ menuAdministrateur(fichier);
+				printf("MODE ADMIN\n");
 				break;
 			
-			case 'c':
-			case 'C':
-				affiche(fichier);
-				break;
-				
-			case 'l':
-			case 'L':
-				lister(fichier);
-				break;
-			
-			case 'o':
-			case 'O':
-				operation(fichier);
+			case '2':
+				//~ menuUtilisateur(fichier);
+				printf("MODE USER\n");
 				break;
 				
 			case 'q':
